9188c90fe58de375dfa25dc1fe9494435b57fe98
tarunsoni10aug <tarunsoni10aug@gmail.com>
2020-06-19 11:43:32 +0530

    connfigServeer

---
+++ SpringBootConfigserver/src/main/java/com/example/demo/SpringBootConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigserverApplication.class, args);
+	}
+
+}
